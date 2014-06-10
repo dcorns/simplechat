b49e14a732d440d9c1ef4800d04a717753804aab
Dale Corns <drcorns@hotmail.com>
2014-06-10 10:54:33 -0700

    Now the file are actually in the commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-simplechat
+simplechat (socket.io, jquery, express)
 ==========
 
 A simple chat application built using express and socket.io
+
+This example is a very minimal and vulnerable chat app that demonstrates using socket.io. Do not use in production.
--- app/main.js
+++ app/main.js
@@ -1,3 +1,45 @@
 /**
  * Created by dcorns on 6/9/14.
  */
+'use strict';
+
+$(function(){
+
+  var users=[], user = '';
+  var socket = io();
+
+  function setUser() {
+    user = $('#userinput').val().trim();
+    $('#inputlabel').text('Enter message:');
+    $('#btn').hide();
+    socket.emit('addclient', user);
+  }
+
+  var chat = (function() {
+    var msg = $('#userinput').val();
+    socket.emit('message', msg);
+  });
+
+
+
+
+
+
+  socket.on('message', function(data){
+    console.log('message'+data);
+    $('.messages').append('data.user says, '+data.msg);
+  });
+
+  socket.on('newuser', function(data){
+    console.log('newuser'+data);
+    users.push(data);
+    console.log(users);
+    $('.userList').append(data);
+  });
+
+  socket.on('usergone', function(data){
+    console.log('usergone'+data);
+    $('.userList').replaceWith(data);
+  });
+
+});
